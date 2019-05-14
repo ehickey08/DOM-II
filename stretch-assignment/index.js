985424a12f1aabf7dabd1d91f6106a63d6524d40
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-05-14 19:05:39 -0400

    Somewhat complete the moving squares for stretch

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,15 +9,42 @@ function toTop (){
     parent.prepend(this);
 }
 
+let isMoving = false;
+let blastRocket = 0;
+let descendRocket = '';
+
+function moveRocket (isMoving, el){
+    if(isMoving){
+        el.style.marginLeft = '10px';
+        blastRocket = setInterval (function () {
+            el.style.marginLeft = parseInt(el.style.marginLeft) + 5 + 'px';
+        },1000/60)
+        
+    } else{
+        
+        descendRocket = setInterval (function () {
+            el.style.marginLeft = parseInt(el.style.marginLeft) - 5 + 'px';
+            if(el.style.marginLeft === '10px'){
+                clearInterval(descendRocket);
+            }
+        },1000/60);
+    }
+}
+
 rockets.forEach(el => {
-    el.addEventListener('mousedown', event => {
+    el.addEventListener('mousedown', function() {
+        isMoving = true;
+        movingBlock = this;
+        moveRocket(isMoving, movingBlock);
     });
 });
 
 
-
-
 rockets.forEach(el => {
-    el.addEventListener('mouseup', event => {
+    el.addEventListener('mouseup', function() {
+        clearInterval(blastRocket);        
+        isMoving = false;
+        movingBlock = this;
+        moveRocket(isMoving, movingBlock);
     });
 });
